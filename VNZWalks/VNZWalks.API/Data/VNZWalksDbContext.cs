872a212d1d3982780131e57d754b15b260f51307
Vinayaka P <vinayakap2024@gmail.com>
2022-10-23 14:53:54 +0530

    End of initial migration with new tables

---
+++ VNZWalks/VNZWalks.API/Data/VNZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VNZWalks.API.Models.Domain;
+
+namespace VNZWalks.API.Data
+{
+    public class VNZWalksDbContext : DbContext
+    {
+        public VNZWalksDbContext(DbContextOptions<VNZWalksDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
